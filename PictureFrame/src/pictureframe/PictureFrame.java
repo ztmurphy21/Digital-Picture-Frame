5c8ba07ff264a75e47cfc0a886de93b8068ae9f8
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-11 21:27:49 -0500

    still not working
    re-arranged some files and got rid of some errors.

--- PictureFrame/src/pictureframe/PictureFrame.java
+++ PictureFrame/src/pictureframe/PictureFrame.java
@@ -3,55 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package digitalpictureframe;
+package pictureframe;
 
-import java.io.File;
-//import java.time.Duration;
-import java.util.Arrays;
-import javafx.util.Duration;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
  * @author Zachary Murphy
  */
-public class DigitalPictureFrame extends Application {
-
-
+public class PictureFrame extends Application {
+    
     @Override
     public void start(Stage primaryStage) {
-          
+         //getting the images
         Image image1 = new Image("1.png");
         Image image2 = new Image("2.png");
         Image image3 = new Image("3.png");
+        //image view needed for image vieiwing 
         ImageView imageView = new ImageView();
+        //new timline
         Timeline timeline = new Timeline(
-
+                //keyframs per image file
                 new KeyFrame(Duration.ZERO, new KeyValue(imageView.imageProperty(), image1)),
             new KeyFrame(Duration.seconds(1), new KeyValue(imageView.imageProperty(), image2)),  
             new KeyFrame(Duration.seconds(2), new KeyValue(imageView.imageProperty(), image3)),
             new KeyFrame(Duration.seconds(4), new KeyValue(imageView.imageProperty(), null))
             );
+        //play method to timeline called
         timeline.play();
+        //setting up stack pane
         StackPane root = new StackPane();
+        
+        //getting all times for the stage.
         root.getChildren().add(imageView);
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
