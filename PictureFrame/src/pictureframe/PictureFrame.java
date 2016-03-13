63f468de95dbef73f96bb257cc38eb4a5b0f3191
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-12 23:06:44 -0500

    finally done
    did some magic tricks... you know..

--- PictureFrame/src/pictureframe/PictureFrame.java
+++ PictureFrame/src/pictureframe/PictureFrame.java
@@ -5,6 +5,7 @@
  */
 package pictureframe;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -12,11 +13,14 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -25,34 +29,48 @@ import javafx.util.Duration;
  * @author Zachary Murphy
  */
 public class PictureFrame extends Application {
-    
+    //array of files
+    String [] imageFiles = {"1.png", "2.png", "3.png"};
+    //some numbers for processing     
+    int i = imageFiles.length;
+     int x =1;
+     //stage 
     @Override
-    public void start(Stage primaryStage) {
-         //getting the images
-        Image image1 = new Image("1.png");
-        Image image2 = new Image("2.png");
-        Image image3 = new Image("3.png");
-        //image view needed for image vieiwing 
-        ImageView imageView = new ImageView();
-        //new timline
-        Timeline timeline = new Timeline(
-                //keyframs per image file
-                new KeyFrame(Duration.ZERO, new KeyValue(imageView.imageProperty(), image1)),
-            new KeyFrame(Duration.seconds(1), new KeyValue(imageView.imageProperty(), image2)),  
-            new KeyFrame(Duration.seconds(2), new KeyValue(imageView.imageProperty(), image3)),
-            new KeyFrame(Duration.seconds(4), new KeyValue(imageView.imageProperty(), null))
-            );
-        //play method to timeline called
-        timeline.play();
-        //setting up stack pane
+    public void start(Stage primaryStage) throws Exception {
+      //label that will hold image
+        Label image = new Label();
+
+      
+        //stack pane set
         StackPane root = new StackPane();
+        //allignment centered
+        root.setAlignment(Pos.CENTER);
+        
+        //handler makes images change
+        EventHandler<ActionEvent> hander= event->{
+            x++;
+            root.getChildren().clear();
+            root.getChildren().addAll(image, new ImageView (imageFiles[x -1]));
+            if (x == i){
+                x=0;
+            }
+        };
+        //durartion per image w/ keyframe
+        Duration sec = new Duration (2000);
+        KeyFrame keyframe = new KeyFrame (sec, hander);
+        //timeline called
+        Timeline timeline = new Timeline(keyframe);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        
         
         //getting all times for the stage.
-        root.getChildren().add(imageView);
+        root.getChildren().addAll(image, new ImageView(imageFiles[x-1]));
         primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("Digital Image Slider");
         primaryStage.show();
+        timeline.playFromStart();
     }
-
+//main method
     public static void main(String[] args) {
         launch(args);
     }
